cd5903a6d8e05beedbb4451d415ce92ed829b5f0
robwilson17 <robert.wilson@livingstoncollegiate.org>
2019-11-22 21:58:25 +0000

    updated runtime

--- projects/runtime/js/level01.js
+++ projects/runtime/js/level01.js
@@ -16,18 +16,21 @@ var level01 = function (window) {
             number: 1, 
             speed: -3,
             gameItems: [
-                {type: 'sawblade',x:440,y:400},
-                {type: 'sawblade',x:500,y:450},
-                {type: 'sawblade',x:800,y:400},
+                {type: 'sawblade',x:440,y:300},
+                {type: 'sawblade',x:520,y:410},
+                {type: 'sawblade',x:800,y:275},
                 {type: 'sawblade',x:900,y:groundY},
-                {type: 'sawblade',x:600,y:345},
-                {type: 'sawblade',x:875,y:340},
-                {type: 'sawblade',x:100,y:329}
+                {type: 'sawblade',x:6780,y:245},
+                {type: 'sawblade',x:875,y:440},
+                {type: 'sawblade',x:200,y:329},
+                {type: 'box',x:500,y:-25},
+                {type: 'enemy',x:400,y:25},
+                {type: 'reward',x:700,y:100}
             ]
         };
         window.levelData = levelData;
         // set this to true or false depending on if you want to see hitzones
-        game.setDebugMode(true);
+        game.setDebugMode(false);
 
         // BEGIN EDITING YOUR CODE HERE
         var hitZoneSize = 25;
@@ -43,36 +46,66 @@ function createSawBlade(x,y) {
             myObstacle.addChild(obstacleImage);            
             obstacleImage.x = -25;
             obstacleImage.y = -25;
+            
     }
 
 var enemy =  game.createGameItem('enemy',25);
+var reward = game.createGameItem('reward', 25);
 var redSquare = draw.rect(50,50,'red');
-redSquare.x = -25;
-redSquare.y = -25;
+var greenSquare = draw.rect(50,50,'green');
+var purpleSquare = draw.rect(50,50,'purple');
+
+//enemy.x = 400;
+//enemy.y = groundY-50;
+//
+//game.addGameItem(enemy);
+function createEnemy(x,y){
+    enemy.x = x;
+    enemy.y = groundY- y;
+    enemy.velocityX = -10;
+    rotationVelocity = 10;
+    enemy.addChild(purpleSquare);
+    
+game.addGameItem(enemy);
+
+var obstacleImage2 = draw.bitmap('img/sawblade.png');          
+            myObstacle.addChild(obstacleImage);            
+            obstacleImage2.x = -25;
+            obstacleImage2.y = -25;
+}
+//createEnemy(800,25)
+function createBox(x,y){
+
+redSquare.x = x;
+redSquare.y = y;
 enemy.addChild(redSquare);
 
-enemy.x = 400;
-enemy.y = groundY-50;
-
-game.addGameItem(enemy);
-function createEnemy(x,y){
+//enemy.x = 400;
+//enemy.y = groundY-50;
+//
+//game.addGameItem(enemy);
+//   
+};
+//createBox(500,-25);
+    enemy.onPlayerCollision = function() {
+        console.log('The enemy has hit Halle');
+        game.changeIntegrity(-100);
+        //enemy.fadeOut();
+};
+function createReward(x, y){
+    greenSquare.x = x;
+    greenSquare.y = y;
+    greenSquare.velocityX = -10;
+    reward.addChild(greenSquare);
+    
     
 }
-
-function createBox(x,y){
-    var myObstacle = game.createObstacle(hitZoneSize,damageFromObstacle);
-            myObstacle.x = x;
-            myObstacle.y = y;
-            game.addGameItem(myObstacle);
-
-        var obstacleImage = draw.bitmap('img/sawblade.png');          
-            myObstacle.addChild(obstacleImage);            
-            obstacleImage.x = -25;
-            obstacleImage.y = -25;
-};
-createBox(100,200);
+    redSquare.onPlayerCollision = function(){
+        game.increaseScore(100);
+        reward.fadeOut();
 
 
+    }
 
         for  (var j = 0; j < levelData.gameItems.length; j++){
             var gameItem = levelData.gameItems[j];
@@ -84,6 +117,9 @@ createBox(100,200);
             }
             if (levelData.gameItems[j].type === 'reward'){
             createReward(gameItem.x, gameItem.y);
+            }
+            if (levelData.gameItems[j].type === 'box'){
+            createBox(gameItem.x,gameItem.y);
             }
         }
         
--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -37,7 +37,7 @@ var background = function (window) {
             // TODO: 2 - Part 2
             // this fills the background with a obnoxious yellow
             // you should modify this to suit your game
-            var backgroundFill = draw.rect(canvasWidth,groundY,'silver');
+            var backgroundFill = draw.rect(canvasWidth,groundY,'black');
             background.addChild(backgroundFill);
             
             // TODO: 3 - Add a moon and starfield
